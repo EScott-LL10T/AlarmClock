060ee4593b3d3cf965c040c190e3612a5e97f3f6
Elliot Scott <elliotscott2111@gmail.com>
2026-06-11 16:53:45 +0100

    utilized printf to correctly format the outputs of date and time without trailing milliseconds.

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -14,7 +14,11 @@ public class AlarmClock implements Runnable{
         while(LocalTime.now().isBefore(alarmTime)){
             try {
                 Thread.sleep(1000);
-                IO.println(LocalTime.now());
+
+                int hours = LocalTime.now().getHour();
+                int minutes = LocalTime.now().getMinute();
+                int seconds = LocalTime.now().getSecond();
+                System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
             } catch (InterruptedException e) {
                 IO.println("Thread was interrupted");
             }
