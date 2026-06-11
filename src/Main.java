0751c5f467877fda449b039588e1d807111eb97a
Elliot Scott <elliotscott2111@gmail.com>
2026-06-11 16:31:58 +0100

    Renamed file containing main method to Main to be able to create a new java class file named AlarmClock.java.

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+class Main {
+    static void main(String[] args){
+        alarmClock();
+    }
+
+    static void alarmClock(){
+        Scanner s = new Scanner(System.in);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        while(alarmTime == null) {
+            try {
+                IO.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = s.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                IO.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                IO.println("invalid format. Please user HH:MM:SS");
+            }
+        }
+
+
+        s.close();
+    }
+}
