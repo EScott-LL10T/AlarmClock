45dedb8baaf64739f02d6a4687e5612310a19f33
Elliot Scott <elliotscott2111@gmail.com>
2026-06-11 16:29:23 +0100

    Added a while loop condition that ensures the program keep requesting the alarm time from the user until a valid time is entered.

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -12,15 +12,17 @@ class AlarmClock{
         Scanner s = new Scanner(System.in);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime alarmTime;
-        try {
-            IO.print("Enter an alarm time (HH:MM:SS): ");
-            String inputTime = s.nextLine();
+        LocalTime alarmTime = null;
+        while(alarmTime == null) {
+            try {
+                IO.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = s.nextLine();
 
-            alarmTime = LocalTime.parse(inputTime, formatter);
-            IO.println("Alarm set for " + alarmTime);
-        } catch (DateTimeParseException e) {
-            IO.println("invalid format. Please user HH:MM:SS");
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                IO.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                IO.println("invalid format. Please user HH:MM:SS");
+            }
         }
 
 
