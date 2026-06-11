3727ccae8f1abce822e2b3e6e3180a137ac5a936
Elliot Scott <elliotscott2111@gmail.com>
2026-06-11 16:23:47 +0100

    Started development of the method called by main alarmClock() that enters a time for the alarm from a user and rejects any DateTimeParseExecptions

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -1,3 +1,29 @@
-void main() {
-    
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+class AlarmClock{
+    static void main(String[] args){
+        alarmClock();
+    }
+
+    static void alarmClock(){
+        Scanner s = new Scanner(System.in);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime;
+        try {
+            IO.print("Enter an alarm time (HH:MM:SS): ");
+            String inputTime = s.nextLine();
+
+            alarmTime = LocalTime.parse(inputTime, formatter);
+            IO.println("Alarm set for " + alarmTime);
+        } catch (DateTimeParseException e) {
+            IO.println("invalid format. Please user HH:MM:SS");
+        }
+
+
+        s.close();
+    }
 }
